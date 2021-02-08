5bf569730d6b28ec3b72881d7c41d1ecc2be3775
Doise <noam.beecomm@gmail.com>
2021-02-09 01:45:37 +0200

    m

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 /**
+ * @fileoverview The app entry point.
+ * @author Noam Kantor
+ */
+
+/**
  * Saying hello.
  * @param {string} name - tell me what's your name.
  */
-export const helloWorld = (name: string): string => {
-  return `Hello ${name}`;
-}
+export const HelloWorld = (name: string): string => {
+  const Hello = 'asd';
+  return `Hello ${name + Hello}`;
+};
